36b9f6d945728fe02886c55e6ea0e126a5ac29a8
LucasMurakami <lucaskaitomurakami@gmail.com>
2023-06-05 14:32:40 -0300

    Service layer, component registration.

--- src/main/java/com/lucasmurakami/course/services/UserService.java
+++ src/main/java/com/lucasmurakami/course/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,5 +17,10 @@ public class UserService {
 
     public List<User> findAll(){
         return repository.findAll();
+    }
+
+    public User findById(long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
     }
 }
